0738d13d9639db7dca8fdaa2f52a39a1d0d05957
514sh <mark.joseph.balagtas@outlook.com>
2025-10-12 23:44:05 +0800

    refactor: backend addtl configs

--- backend/goxave/config.py
+++ backend/goxave/config.py
@@ -4,15 +4,21 @@ from pymongo import MongoClient
 
 from goxave.api.service_layer import unit_of_work
 
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_NAME = os.environ["DB_NAME"]
 DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_REPLICASET = os.environ["DB_REPLICASET"]
+DB_AUTH_SOURCE = os.environ["DB_AUTH_SOURCE"]
 
 REDIS_PORT = os.environ["REDIS_PORT"]
 REDIS_HOST = os.environ["REDIS_HOST"]
 FIREBASE_AUTH_PROJECT_ID = os.environ["FIREBASE_AUTH_PROJECT_ID"]
 PROXY_URL = os.environ["PROXY_URL"]
+
+INTERVAL_HOUR = os.environ["INTERVAL_HOUR"]
+INTERVAL_MIN = os.environ["INTERVAL_MIN"]
 
 
 _client = None
@@ -21,7 +27,14 @@ _client = None
 def get_database_client():
     global _client
     if _client is None:
-        _client = MongoClient(host=DB_HOST, port=int(DB_PORT), replicaSet=DB_REPLICASET)
+        _client = MongoClient(
+            host=DB_HOST,
+            port=int(DB_PORT),
+            replicaSet=DB_REPLICASET,
+            authSource=DB_AUTH_SOURCE,
+            username=DB_USERNAME,
+            password=DB_PASSWORD,
+        )
     return _client
 
 
