420c729788795a729a4f7b8618f54b5de0e49885
514sh <mark.joseph.balagtas@outlook.com>
2025-10-19 09:59:20 +0800

    fix: backend - limiter - storage_uri

--- backend/goxave/common.py
+++ backend/goxave/common.py
@@ -20,6 +20,6 @@ from slowapi.util import get_remote_address
 limiter = Limiter(
     key_func=get_remote_address,
     strategy="fixed-window",
-    storage_uri="redis://{REDIS_HOST}:{REDIS_PORT}",
+    storage_uri=f"redis://{REDIS_HOST}:{REDIS_PORT}",
     enabled=not bool(RATE_LIMIT_DISABLED),
 )
