53b28d2491aaef2112fd3e9296abd8792ae17312
514sh <mark.joseph.balagtas@outlook.com>
2025-10-20 23:20:55 +0800

    feat: web - constants.ts

---
+++ web/src/constants.ts
@@ -0,0 +1,26 @@
+export const stores = [
+  {
+    id: 1,
+    name: "Amazon",
+    url: "https://www.amazon.com",
+    logo: "https://logo.clearbit.com/amazon.com",
+  },
+  {
+    id: 2,
+    name: "Datablitz",
+    url: "https://ecommerce.datablitz.com.ph",
+    logo: "https://logo.clearbit.com/ecommerce.datablitz.com.ph",
+  },
+  {
+    id: 3,
+    name: "JBL Store PH",
+    url: "https://jblstore.com.ph/",
+    logo: "https://logo.clearbit.com/jblstore.com.ph",
+  },
+  {
+    id: 4,
+    name: "Lazada",
+    url: "https://www.lazada.com.ph/",
+    logo: "https://logo.clearbit.com/lazada.com.ph/",
+  },
+];
